0acd66632ea701e2470030fb1dacc018e8c278db
Tanvir Anowar <tanvir.net@gmail.com>
2021-05-03 18:20:02 +0100

    Link List in progress.

---
+++ ListCustom/LinkList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListCustom
+{
+    public class LinkList
+    {
+        private class Node
+        {
+            private int Value { get; set; }
+            private Node NextNode { get; set; }
+
+            public Node(int value)
+            {
+                Value = value;
+            }
+
+            public void SetNextNode(Node currentNode)
+            {
+                NextNode = currentNode;
+            }
+        }
+
+        private Node FirstNode { get; set; }
+        private Node LastNode { get; set; }
+
+        public void Add(int item)
+        {
+            var itemNode = new Node(item);
+
+            if (FirstNode == null)
+            {
+                FirstNode = itemNode;
+                LastNode = itemNode;
+            }
+            else 
+            {
+                LastNode.SetNextNode(itemNode);
+            }
+
+
+
+        }
+    }
+
+    
+}
---
+++ ListCustom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListCustom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
