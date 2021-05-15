7331f6adbbaff6a08010ba144cb12d113914f589
Tanvir Anowar <tanvir.net@gmail.com>
2021-05-15 20:33:04 +0100

    Stack inprogress

---
+++ StackCustom/StackCustom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackCustom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
